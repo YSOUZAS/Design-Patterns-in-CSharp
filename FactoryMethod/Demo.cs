7a6aaa07cbd0473476c4781cdee03ae266f2ea3e
ysouzas <yago.souza.santos@gmail.com>
2018-01-15 14:08:16 -0200

    add Factory Method

---
+++ FactoryMethod/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.FactoryMethod
+{
+    public class Demo
+    {
+        public Demo()
+        {
+        }
+
+        public void Show()
+        {
+            var a = ProductFactory.CreateProduct("A");
+            var b = ProductFactory.CreateProduct("B");
+
+            Console.WriteLine(a.Method());
+            Console.WriteLine(b.Method());
+
+        }
+    }
+}
---
+++ FactoryMethod/FactoryMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.FactoryMethod
+{
+    public interface IAbstractPorduct
+    {
+        string Method();
+    }
+
+    public class ConcreteProductA : IAbstractPorduct
+    {
+        public string Method() => "Method of ConcreteProductA";
+    }
+
+    public class ConcreteProductB : IAbstractPorduct
+    {
+        public string Method() => "Method of ConcreteProductB";
+    }
+
+    public class ProductFactory
+    {
+        public static IAbstractPorduct CreateProduct(string type)
+        {
+            if (type == "A")
+                return new ConcreteProductA();
+            else if (type == "B")
+                return new ConcreteProductB();
+
+            return null;
+        }
+    }
+
+}
