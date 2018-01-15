b6c31f1da98e2966c0601a241c2d0cde98598dac
ysouzas <yago.souza.santos@gmail.com>
2018-01-15 14:52:28 -0200

    add Interpreter

---
+++ Interpreter/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Design_Patterns_in_CSharp.Interpreter
+{
+    public class Demo
+    {
+        public Demo()
+        {
+        }
+
+        public void Show()
+        {
+
+            var context = new Context();
+            var list = new LinkedList<AbstractExpression>();
+
+            list.AddLast(new NonterminalExpression());
+            list.AddLast(new NonterminalExpression());
+            list.AddLast(new NonterminalExpression());
+            list.AddLast(new TerminalExpression());
+            list.AddLast(new NonterminalExpression());
+            list.AddLast(new NonterminalExpression());
+            list.AddLast(new TerminalExpression());
+            list.AddLast(new TerminalExpression());
+
+            foreach (var item in list)
+            {
+                item.Interpret(context);
+            }
+        }
+    }
+}
---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.Interpreter
+{
+    public class Context
+    {
+
+    }
+
+    public interface AbstractExpression
+    {
+        void Interpret(Context context);
+    }
+
+    public class TerminalExpression : AbstractExpression
+    {
+        public void Interpret(Context context) => Console.WriteLine("`interpret` method of TerminalExpression is being called!");
+    }
+
+    public class NonterminalExpression : AbstractExpression
+    {
+        public void Interpret(Context context) => Console.WriteLine("`interpret` method of NonterminalExpression is being called!");
+    }
+
+}
