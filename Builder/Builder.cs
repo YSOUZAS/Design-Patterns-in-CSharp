1ecfaa3520bd686267a5f44151a23c5e0b1f5652
ysouzas <yago.souza.santos@gmail.com>
2018-01-17 17:46:09 -0200

    add Builder

---
+++ Builder/Builder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.Builder
+{
+
+    public class UserBuilder
+    {
+        private string name;
+        private int age;
+        private string phone;
+        private string address;
+
+        public UserBuilder(string name)
+        {
+            this.name = name;
+        }
+
+        public User Build() => new User(this);
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Address { get => address; set => address = value; }
+    }
+
+    public class User
+    {
+
+        private readonly string name;
+        private readonly int age;
+        private readonly string phone;
+        private readonly string address;
+
+        public string Name { get => name; }
+        public int Age { get => age; }
+        public string Phone { get => phone; }
+        public string Address { get => address; }
+
+
+
+        public User(UserBuilder builder)
+        {
+            this.name = builder.Name;
+            this.age = builder.Age;
+            this.phone = builder.Phone;
+            this.address = builder.Address;
+        }
+
+    }
+
+
+
+
+
+}
---
+++ Builder/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.Builder
+{
+    public class Demo
+    {
+        public Demo()
+        {
+        }
+
+        public void Show()
+        {
+            var builder = new UserBuilder("Jancsi");
+            builder.Age = 12;
+            builder.Phone = "0123456789";
+            builder.Address = "asdf";
+            var user = builder.Build();
+
+            Console.WriteLine(user.Name + " " + user.Age + " " + user.Phone + " " + user.Address);
+
+        }
+    }
+}
