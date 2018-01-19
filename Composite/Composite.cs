2dfd6a25b946f5910850a1a15a36c78dca77c09e
ysouzas <yago.souza.santos@gmail.com>
2018-01-19 17:36:45 -0200

    add Composite

---
+++ Composite/Composite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Design_Patterns_in_CSharp.Composite
+{
+
+    public interface IComponent
+    {
+        void Operation();
+    }
+
+    public class Composite : IComponent
+    {
+        private LinkedList<IComponent> list;
+        private string s;
+
+        public Composite(string s)
+        {
+            this.list = new LinkedList<IComponent>();
+            this.s = s;
+        }
+
+        public void Operation()
+        {
+            Console.WriteLine("`operation of `" + this.s);
+            foreach (var item in this.list)
+            {
+                item.Operation();
+            }
+        }
+
+        public void Add(IComponent c) => this.list.AddLast(c);
+
+        public void Remove(IComponent c) => this.list.Remove(c);
+    }
+    public class Leaf : IComponent
+    {
+        private string s;
+
+        public Leaf(string s) => this.s = s;
+
+        public void Operation() => Console.WriteLine("`operation` of Leaf " + this.s + " is called.");
+    }
+}
+
---
+++ Composite/Demo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Design_Patterns_in_CSharp.Composite
+{
+    public class Demo
+    {
+        public Demo()
+        {
+        }
+
+        public void Show()
+        {
+            var leaf1 = new Leaf("1");
+            var leaf2 = new Leaf("2");
+            var leaf3 = new Leaf("3");
+
+            var composite1 = new Composite("Comp1");
+            var composite2 = new Composite("Comp2");
+
+
+            composite1.Add(leaf1);
+            composite1.Add(leaf2);
+            composite1.Add(leaf3);
+
+            composite1.Add(leaf2);
+
+            composite2.Add(leaf1);
+            composite2.Add(leaf3);
+
+            composite1.Operation();
+            composite1.Operation();
+
+
+
+        }
+    }
+}
